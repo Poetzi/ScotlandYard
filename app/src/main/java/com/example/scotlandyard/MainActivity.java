1314c4d97425f638694c9e9131d5a43f86ff89e4
Patrick <patrick.petzmann@gmail.com>
2020-04-03 16:26:46 +0200

    Kommentar wurde in MainActivity hinzugefügt

--- app/src/main/java/com/example/scotlandyard/MainActivity.java
+++ app/src/main/java/com/example/scotlandyard/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Kommentar1
+    // Kommentar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
