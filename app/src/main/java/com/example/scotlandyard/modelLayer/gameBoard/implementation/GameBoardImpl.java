eb589acc3cd1a01946db6ee30e368dbadf6b15c8
Stephan <p.stephan@gmx.at>
2020-04-09 17:56:46 +0200

    Details in Struktur eingefügt

--- app/src/main/java/com/example/scotlandyard/modelLayer/gameBoard/implementation/GameBoardImpl.java
+++ app/src/main/java/com/example/scotlandyard/modelLayer/gameBoard/implementation/GameBoardImpl.java
@@ -3,7 +3,24 @@ package com.example.scotlandyard.modelLayer.gameBoard.implementation;
 import com.example.scotlandyard.modelLayer.gameBoard.interfaces.GameBoard;
 import com.example.scotlandyard.modelLayer.transitions.interfaces.Transition;
 
+import java.util.HashSet;
+
 public class GameBoardImpl implements GameBoard {
+
+    /*
+       Jede Stelle im Arrray steht für einen Spieler
+     */
+    int [] playersPositions;
+    /*
+        Speichert die Anzahl der Spielfelder
+     */
+    HashSet<Integer> fields; //(Liste oder Set von Field)
+    /*
+        Speichert die Wege von einem Spielfeld zum anderen
+     */
+    HashSet<Transition> transitions; //(Liste oder Set)
+
+
     @Override
     public void addFieldWithTransition(int fromField, int toField, Transition Rule) {
 
