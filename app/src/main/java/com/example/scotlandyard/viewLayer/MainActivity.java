df610320e5137045bf0a67a178f2c958add2b3a4
Patrick <patrick.petzmann@gmail.com>
2020-04-14 09:41:04 +0200

    Ein Package für Presenter Layer und View Layer wurde hinzugefügt

--- app/src/main/java/com/example/scotlandyard/viewLayer/MainActivity.java
+++ app/src/main/java/com/example/scotlandyard/viewLayer/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.scotlandyard;
+package com.example.scotlandyard.viewLayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.scotlandyard.R;
 
 public class MainActivity extends AppCompatActivity {
 
