e4108fbc702f3da46572fcac4e88e8e065ddd272
Patrick <patrick.petzmann@gmail.com>
2020-04-14 09:30:07 +0200

    Die Klasse MyKryoCLient wurde implementiert

--- app/src/main/java/com/example/scotlandyard/client/MyKryoClient.java
+++ app/src/main/java/com/example/scotlandyard/client/MyKryoClient.java
@@ -1,4 +1,38 @@
 package com.example.scotlandyard.client;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Client;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class MyKryoClient {
+    private Client client;
+    private MyClientListener listener;
+    private Kryo kryo;
+
+    public MyKryoClient() {
+        client = new Client();
+        listener = new MyClientListener();
+        client.addListener(listener);
+        listener.init(client);
+        kryo = client.getKryo();
+        registerMessages();
+    }
+
+
+    public void connectToServer(String host) throws IOException {
+        client.start();
+        client.connect(5000, host, Ports.TCP);
+    }
+
+    public void stopClient() {
+        client.stop();
+    }
+
+    private void registerMessages() {
+
+    }
+
 }
+
