4186155216a04f855d54f88f737e71c1e27114e4
Patrick <patrick.petzmann@gmail.com>
2020-04-17 11:03:22 +0200

    eine Methode um eine Verbindung mit dem Server aufzubauen wurde in NetworkTestActivity implementiert und getestet

--- app/src/main/java/com/example/scotlandyard/viewLayer/NetworkTestActivity.java
+++ app/src/main/java/com/example/scotlandyard/viewLayer/NetworkTestActivity.java
@@ -3,14 +3,24 @@ package com.example.scotlandyard.viewLayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.scotlandyard.R;
+import com.example.scotlandyard.client.MyNetworkThread;
 
 public class NetworkTestActivity extends AppCompatActivity {
+
+    private MyNetworkThread networkThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_network_test);
+        networkThread = new MyNetworkThread();
+    }
+
+    public void connectToServer(View view)
+    {
+        networkThread.start();
     }
 }
