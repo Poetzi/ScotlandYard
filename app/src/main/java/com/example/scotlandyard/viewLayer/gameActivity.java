a1351ec67ca7fc3623ab8e0692135e84a87255ac
nilsk <nils.kopali20@live.com>
2020-05-07 12:41:26 +0200

    Trying to get X and Y positions from Map to add points. Getting incorrect X and Y.

--- app/src/main/java/com/example/scotlandyard/viewLayer/gameActivity.java
+++ app/src/main/java/com/example/scotlandyard/viewLayer/gameActivity.java
@@ -3,6 +3,8 @@ package com.example.scotlandyard.viewLayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 
 import com.example.scotlandyard.R;
 
@@ -13,4 +15,18 @@ public class gameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            float x = event.getX();
+            float y = event.getY();
+
+
+            Log.i("Coordinate", "X-" + x + "===Y=" + y);
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/example/scotlandyard/viewLayer/mapView.java
+++ app/src/main/java/com/example/scotlandyard/viewLayer/mapView.java
@@ -5,7 +5,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -15,6 +19,7 @@ import com.example.scotlandyard.R;
 public class mapView extends View {
 
     Bitmap small;
+    Paint paint = new Paint();
 
     public mapView(Context context) {
         super(context);
@@ -37,6 +42,15 @@ public class mapView extends View {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
         canvas.drawBitmap(small,0,0,null);
+        Bitmap f1 = BitmapFactory.decodeResource(getResources(),R.drawable.field1);
+        canvas.drawBitmap(f1,17,178,null);
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(15f);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        canvas.drawLine(22,181,76,375,paint);
+
     }
 
 }
