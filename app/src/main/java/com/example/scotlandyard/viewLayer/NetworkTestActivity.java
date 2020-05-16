96119698a05e5bcdeda04615238fb83edf80fbe5
Poetzi <49530417+Poetzi@users.noreply.github.com>
2020-05-16 13:01:38 +0200

    Merge branch 'develop' of C:\Users\pstep\OneDrive\Uni\Sommersemester2020\SE2\ScotlandYard with conflicts.

--- app/src/main/java/com/example/scotlandyard/viewLayer/NetworkTestActivity.java
+++ app/src/main/java/com/example/scotlandyard/viewLayer/NetworkTestActivity.java
@@ -3,16 +3,19 @@ package com.example.scotlandyard.viewLayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.scotlandyard.R;
+import com.example.scotlandyard.client.Message;
 import com.example.scotlandyard.client.MyNetworkThread;
 
 public class NetworkTestActivity extends AppCompatActivity {
 
     private MyNetworkThread networkThread;
     private TextView textView;
+    private TextView sendenTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,22 @@ public class NetworkTestActivity extends AppCompatActivity {
     public void connectToServer(View view)
     {
         networkThread.start();
+    }
+
+    public void sendMessage(View view){
+        sendenTextView = findViewById(R.id.sendenTextFeld);
+        String buffer = "" + sendenTextView.getText().toString();
+        Message message = new Message();
+        message.message = buffer;
+
+        //enableStrictMode();
+        networkThread.sendenMethode(message);
+    }
+
+    public void enableStrictMode()
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
     }
 }
