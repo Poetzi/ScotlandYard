dc49c2d2b2c9bc252c4c1d2b2e5c87f670d966cc
Patrick <patrick.petzmann@gmail.com>
2020-05-29 11:23:45 +0200

    [FinalUI] Fehler in BoardGameEngine behoben

--- server/src/main/java/com/example/server/game/boardGameEngine/implementation/BoardGameEngineImpl.java
+++ server/src/main/java/com/example/server/game/boardGameEngine/implementation/BoardGameEngineImpl.java
@@ -36,7 +36,7 @@ public class BoardGameEngineImpl implements BoardGameEngine {
          * new MrXImpl(name,players.size()); }else { player=new
          * DetectiveImpl(name,players.size()); } player.setCurrentPosition(field);
          * players.add(player);
-         * 
+         *
          * gameBoard.setStartField(players.size(),field);
          */
         Player player = new PlayerImpl();
@@ -84,7 +84,7 @@ public class BoardGameEngineImpl implements BoardGameEngine {
     @Override
     public void drawForPlayer(Player player) {
         //Zug wird nur durchgeführt, wenn der Detektiv nicht inaktiv ist oder wenn es sich um den Zug von Mr. X handelt.
-        if ((player instanceof Detective && ((Detective)player).isInactive()==false) || player instanceof MrX) {
+        if ((player instanceof Detective && ((Detective) player).isInactive() == false) || player instanceof MrX) {
             String card = "Bus";    // Beispielwert
             int fieldToGo = 0;
             boolean drawValide = false;
@@ -96,42 +96,116 @@ public class BoardGameEngineImpl implements BoardGameEngine {
                Der Server holt sich vom Spieler Client die Karte die er einsetzen will
                und die Position zu der er ziehen möchte
             */
-            turnMessage = lobby.askPlayerforTurn(player.getId());
-            card = turnMessage.getCard();
-            fieldToGo = turnMessage.getToField();
-        System.out.println(card+"sssss "+ fieldToGo);
+                turnMessage = lobby.askPlayerforTurn(player.getId());
+                card = turnMessage.getCard();
+                fieldToGo = turnMessage.getToField();
+                System.out.println(card + "sssss " + fieldToGo);
             /*
                Die Daten vom Zug des Spielers werden weitergegeben an das Gameboard wo überprüft wird,
                ob der Zug gültig ist.
                Wenn der Zug nicht gültig ist wird ein neuer Zug vom Spieler abgefragt.
             */
-            if (gameBoard.checkDraw(player.getId(),fieldToGo,card))
-            {
-                lobby.confirm(player.getId(),"yes");
-               // break;
-                drawValide = true;
-            }else {
-                lobby.confirm(player.getId(),"no");
+                if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
+                    lobby.confirm(player.getId(), "yes");
+                    // break;
+                    drawValide = true;
+                } else {
+                    lobby.confirm(player.getId(), "no");
 
-            }
-       // }
+                }
+                // }
 
         /*
             Dem Spieler muss die verwendete Karte noch aus seinen verfügbaren Karten entfernt werden
          */
-            Transition toRemove = new TransitionImpl();
-            toRemove.setName(card);
-            player.removeTransitionFromAvailable(toRemove);
+                Transition toRemove = new TransitionImpl();
+                toRemove.setName(card);
+                player.removeTransitionFromAvailable(toRemove);
 
-            if (player instanceof Detective) {
-                //Wenn der Detektiv den Verdacht äußert, dass Mr. X geschummelt hat.
-                if (card.equals("cheat")){
-                    if(checkIfMrXCheated()){
-                        int misterX = players.size() - 1;
-                        ((MrX)(players.get(misterX))).setCaughtCheating(true,actualRound);
-                        ((MrX)(players.get(misterX))).setVisibleFor(2);
+                if (player instanceof Detective) {
+                    //Wenn der Detektiv den Verdacht äußert, dass Mr. X geschummelt hat.
+                    if (card.equals("cheat")) {
+                        if (checkIfMrXCheated()) {
+                            int misterX = players.size() - 1;
+                            ((MrX) (players.get(misterX))).setCaughtCheating(true, actualRound);
+                            ((MrX) (players.get(misterX))).setVisibleFor(2);
 
-                        //Detektiv darf seine Position ändern.
+                            //Detektiv darf seine Position ändern.
+                            drawValide = false;
+                            // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
+                            while (drawValide = false) {
+
+                                turnMessage = lobby.askPlayerforTurn(player.getId());
+                                card = turnMessage.getCard();
+                                fieldToGo = turnMessage.getToField();
+
+                                if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
+                                    drawValide = true;
+                                }
+                            }
+                            ((Detective) player).validateTicket(card);
+
+                        } else {
+                            //Wenn Mr. X nicht geschummelt hat, wird der Detektiv für diese und  nächste Runde als inaktiv gekennzeichnet.
+                            ((Detective) player).setInactive(true);
+                            fieldToGo = player.getCurrentPosition();
+                        }
+                    } else {
+                        ((Detective) player).validateTicket(card);
+                    }
+                } else if (player instanceof MrX) {
+
+                    if (!card.equals("Double") && !card.equals("cheat")) {
+                        //Normaler Zug von Mister X
+                        ((MrX) player).validateTicket(actualRound, card, fieldToGo);
+
+                        if (((MrX) player).isCaughtCheating()) {
+                            ((MrX) player).setCaughtCheating(true, actualRound);
+                        }
+                        lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
+                    } else if (card.equals("cheat")) {
+
+                        drawValide = false;
+                        // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
+                        while (drawValide = false) {
+
+                            turnMessage = lobby.askPlayerforTurn(player.getId());
+                            card = turnMessage.getCard();
+                            fieldToGo = turnMessage.getToField();
+
+
+                            if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
+                                drawValide = true;
+                            }
+                        }
+                        //Erster Zug wird normal im Travellog gespeichert.
+                        ((MrX) player).validateTicket(actualRound, "cheat", 0); //Position ist hier egal
+                        ((MrX) player).validateTicket(actualRound, card, fieldToGo);
+                        if (((MrX) player).isCaughtCheating()) {
+                            ((MrX) player).setCaughtCheating(true, actualRound);
+                        }
+                        lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
+
+                        drawValide = false;
+                        // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
+                        while (drawValide = false) {
+
+                            turnMessage = lobby.askPlayerforTurn(player.getId());
+                            card = turnMessage.getCard();
+                            fieldToGo = turnMessage.getToField();
+
+
+                            if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
+                                drawValide = true;
+                            }
+                        }
+                        //Zweiter, geschummelter Zug wird nicht im Travellog gespeichert und ist somit nicht sichtbar für Detektive.
+                        player.setCurrentPosition(fieldToGo);
+
+                    } else {
+                        /**
+                         *      Warten bis Spieler den ersten Zug des Doppelzugs macht.
+                         */
                         drawValide = false;
                         // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
                         while (drawValide = false) {
@@ -144,123 +218,48 @@ public class BoardGameEngineImpl implements BoardGameEngine {
                                 drawValide = true;
                             }
                         }
-                        ((Detective) player).validateTicket(card);
-
-                    }else {
-                        //Wenn Mr. X nicht geschummelt hat, wird der Detektiv für diese und  nächste Runde als inaktiv gekennzeichnet.
-                        ((Detective)player).setInactive(true);
-                        fieldToGo=player.getCurrentPosition();
-                    }
-                }else {
-                    ((Detective) player).validateTicket(card);
-                }
-            } else if (player instanceof MrX) {
-
-                if (!card.equals("Double") && !card.equals("cheat")) {
-                    //Normaler Zug von Mister X
-                    ((MrX) player).validateTicket(actualRound, card, fieldToGo);
-
-                    if (((MrX)player).isCaughtCheating()){
-                        ((MrX)player).setCaughtCheating(true,actualRound);
-                    }
-                    lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
-                } else if (card.equals("cheat")) {
-
-                    drawValide = false;
-                    // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
-                    while (drawValide = false) {
-
-                        turnMessage = lobby.askPlayerforTurn(player.getId());
-                        card = turnMessage.getCard();
-                        fieldToGo = turnMessage.getToField();
-
-
-                       if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
-                            drawValide = true;
+                        //Erster Zug wird im Travellog normal gespeichert.
+                        ((MrX) player).validateDoubleMoveTicket(actualRound, card, fieldToGo);
+                        if (((MrX) player).isCaughtCheating()) {
+                            ((MrX) player).setCaughtCheating(false, actualRound);
                         }
-                    }
-                    //Erster Zug wird normal im Travellog gespeichert.
-                    ((MrX) player).validateTicket(actualRound, "cheat", 0); //Position ist hier egal
-                    ((MrX) player).validateTicket(actualRound, card, fieldToGo);
-                    if (((MrX)player).isCaughtCheating()){
-                        ((MrX)player).setCaughtCheating(true,actualRound);
-                    }
-                    lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
+                        lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
 
-                    drawValide = false;
-                    // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
-                    while (drawValide = false) {
+                        /**
+                         *      Warten auf den zweiten Zug des Doppelzugs.
+                         */
+                        drawValide = false;
+                        // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
+                        while (drawValide = false) {
 
-                        turnMessage = lobby.askPlayerforTurn(player.getId());
-                        card = turnMessage.getCard();
-                        fieldToGo = turnMessage.getToField();
+                            turnMessage = lobby.askPlayerforTurn(player.getId());
+                            card = turnMessage.getCard();
+                            fieldToGo = turnMessage.getToField();
 
-
-                        if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
-                            drawValide = true;
+                            if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
+                                drawValide = true;
+                            }
                         }
-                    }
-                    //Zweiter, geschummelter Zug wird nicht im Travellog gespeichert und ist somit nicht sichtbar für Detektive.
-                    player.setCurrentPosition(fieldToGo);
-
-                } else {
-                    /**
-                     *      Warten bis Spieler den ersten Zug des Doppelzugs macht.
-                     */
-                    drawValide = false;
-                    // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
-                    while (drawValide = false) {
-
-                        turnMessage = lobby.askPlayerforTurn(player.getId());
-                        card = turnMessage.getCard();
-                        fieldToGo = turnMessage.getToField();
-
-                        if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
-                            drawValide = true;
+                        //Zweiter Zug wird im Travellog normal gespeichert.
+                        ((MrX) player).validateTicket(actualRound, card, fieldToGo);
+                        if (((MrX) player).isCaughtCheating()) {
+                            ((MrX) player).setCaughtCheating(false, actualRound);
                         }
+                        lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
                     }
-                    //Erster Zug wird im Travellog normal gespeichert.
-                    ((MrX) player).validateDoubleMoveTicket(actualRound, card, fieldToGo);
-                    if (((MrX)player).isCaughtCheating()){
-                        ((MrX)player).setCaughtCheating(false,actualRound);
-                    }
-                    lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
-
-                    /**
-                     *      Warten auf den zweiten Zug des Doppelzugs.
-                     */
-                    drawValide = false;
-                    // Schleife wird solange ausgeführt bis ein gültiger Zug vom Spieler kommt
-                    while (drawValide = false) {
-
-                        turnMessage = lobby.askPlayerforTurn(player.getId());
-                        card = turnMessage.getCard();
-                        fieldToGo = turnMessage.getToField();
-
-                        if (gameBoard.checkDraw(player.getId(), fieldToGo, card)) {
-                            drawValide = true;
-                        }
-                    }
-                    //Zweiter Zug wird im Travellog normal gespeichert.
-                    ((MrX) player).validateTicket(actualRound, card, fieldToGo);
-                    if (((MrX)player).isCaughtCheating()){
-                        ((MrX)player).setCaughtCheating(false,actualRound);
-                    }
-                    lobby.updateTravellogToAllClients(((MrX) player).getTravelLog(actualRound), actualRound);
-                }
                 /*
                     Wenn Mister X beim Schummeln erwischt wurde, wird hier heruntergezählt,
                     wie lange seine Position noch für die Detektive sichtbar ist.
                  */
-                if (((MrX)player).isCaughtCheating()){
-                    ((MrX)player).setVisibleFor(((MrX)player).getVisibleFor()-1);
-                    if (((MrX)player).getVisibleFor()==0){
-                        ((MrX)player).setCaughtCheating(false,actualRound);
+                    if (((MrX) player).isCaughtCheating()) {
+                        ((MrX) player).setVisibleFor(((MrX) player).getVisibleFor() - 1);
+                        if (((MrX) player).getVisibleFor() == 0) {
+                            ((MrX) player).setCaughtCheating(false, actualRound);
+                        }
                     }
-                }
 
-            }
-            player.setCurrentPosition(fieldToGo);
+                }
+                player.setCurrentPosition(fieldToGo);
 
 
 
@@ -268,12 +267,13 @@ public class BoardGameEngineImpl implements BoardGameEngine {
             Wenn der Zug gültig ist, wird die Positon des Spielers auf dem Gameboard gesetzt
             und an die anderen Spieler Clients weitergegeben
          */
-            if (drawValide) {
-                gameBoard.setPositionOfPlayer(player.getId(), fieldToGo);
-                lobby.updatePlayerPositionsToAllClients(player.getId(), fieldToGo);
+                if (drawValide) {
+                    gameBoard.setPositionOfPlayer(player.getId(), fieldToGo);
+                    lobby.updatePlayerPositionsToAllClients(player.getId(), fieldToGo);
+                } else {
+                    ((Detective) player).setInactive(false);
+                }
             }
-        else {
-            ((Detective)player).setInactive(false);
         }
     }
 
@@ -288,7 +288,7 @@ public class BoardGameEngineImpl implements BoardGameEngine {
 
         for (int i = 0; i < numberOfPlayers; i++) {
             if (players.get(misterX).getCurrentPosition() == players.get(i).getCurrentPosition()) { // Missing field
-                                                                                                    // from Player CLass
+                // from Player CLass
                 System.out.println("Mister X lost");
                 return true;
             }
@@ -296,30 +296,34 @@ public class BoardGameEngineImpl implements BoardGameEngine {
         return false;
     }
 
-    public void initLobby(Lobby lobby) {
-        this.lobby = lobby;
-    }
 
-    public void setLobby(Lobby lobby) {
-        this.lobby = lobby;
-    }
 
-    /**
-     * Methode überprüft ob Mr. X in den letzten 5 Zügen mindestens einmal
-     * geschummelt hat.
-     * 
-     * @return true, wenn mindestens einmal {@link MrX#getHasCheatedInRound(int)}
-     *         true zurück liefert.
-     */
-    @Override
-    public boolean checkIfMrXCheated() {
-        int misterX = players.size() - 1;
-        MrX mrX = (MrX) players.get(misterX);
-        for (int i = actualRound - 5; i <= actualRound; i++) {
-            if (mrX.getHasCheatedInRound(i)) {
-                return true;
+            public void initLobby (Lobby lobby){
+                this.lobby = lobby;
+            }
+
+            public void setLobby (Lobby lobby){
+                this.lobby = lobby;
+            }
+
+            /**
+             * Methode überprüft ob Mr. X in den letzten 5 Zügen mindestens einmal
+             * geschummelt hat.
+             *
+             * @return true, wenn mindestens einmal {@link MrX#getHasCheatedInRound(int)}
+             *         true zurück liefert.
+             */
+            @Override
+            public boolean checkIfMrXCheated () {
+                int misterX = players.size() - 1;
+                MrX mrX = (MrX) players.get(misterX);
+                for (int i = actualRound - 5; i <= actualRound; i++) {
+                    if (mrX.getHasCheatedInRound(i)) {
+                        return true;
+                    }
+                }
+                return false;
             }
         }
-        return false;
-    }
-}
+
+
