76ea67c7438fa3e4586a65d88c2a47523339e8d5
Poetzi <49530417+Poetzi@users.noreply.github.com>
2020-06-05 16:50:31 +0200

    Chatfunktion überarbeitet, Funktionalität von Presenter ausgelagert.

--- app/src/main/java/com/example/scotlandyard/playActivity.java
+++ app/src/main/java/com/example/scotlandyard/playActivity.java
@@ -1,25 +1,15 @@
 package com.example.scotlandyard;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.scotlandyard.presenterLayer.Presenter;
 import com.example.scotlandyard.viewLayer.Chat;
 import com.example.scotlandyard.viewLayer.IPFinder;
 import com.example.scotlandyard.viewLayer.gameActivity;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 
 public class playActivity extends AppCompatActivity {
     private Presenter presenter = Presenter.getInstance();
@@ -45,12 +35,16 @@ public class playActivity extends AppCompatActivity {
             IPFinder ipFinder=new IPFinder(getApplicationContext());
             ipFinder.findIP();
             //Server wird gestartet.
-            presenter.connectToServer("143.205.186.73");
+            presenter.connectToServer("143.205.187.52");
             presenter.sendUsername();
+
 
         }).start();
 
+        //Intent wird gestartet um durch Knopfdruck auf Chat-Seite zu gelangen
         Intent intent = new Intent(this, gameActivity.class);
         startActivity(intent);
+
+
     }
 }
