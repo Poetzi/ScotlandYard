6a831f72ce2ef84b4c7c3ac08271835b9cd469df
Patrick <patrick.petzmann@gmail.com>
2020-06-05 17:04:12 +0200

    [Develop2] Fixed Lobby never is opened

--- server/src/main/java/com/example/server/lobby/implementation/LobbyImpl.java
+++ server/src/main/java/com/example/server/lobby/implementation/LobbyImpl.java
@@ -22,7 +22,7 @@ public class LobbyImpl implements Lobby {
 
 
     private ArrayList<ID> players = new ArrayList<ID>();
-    private boolean isOpen = false;
+    private boolean isOpen = true;
     public int playerCount = 0;
 
     private BoardGameEngine game = new BoardGameEngineImpl();
