9127db510bd17810946acf253f6e3827404cfd14
Patrick <patrick.petzmann@gmail.com>
2020-06-05 17:53:39 +0200

    [Develop2] LobbyId wurde zur SendRoleMessage hinzugefügt

--- app/src/main/java/com/example/scotlandyard/Client/Messages/SendRoleMessage.java
+++ app/src/main/java/com/example/scotlandyard/Client/Messages/SendRoleMessage.java
@@ -3,6 +3,16 @@ package com.example.scotlandyard.Client.Messages;
 public class SendRoleMessage extends BaseMessage {
     private String text;
 
+    private int lobbyID;
+
+    public int getLobbyID() {
+        return lobbyID;
+    }
+
+    public void setLobbyID(int lobbyID) {
+        this.lobbyID = lobbyID;
+    }
+
     public String getText() {
         return text;
     }
--- server/src/main/java/com/example/server/messages/SendRoleMessage.java
+++ server/src/main/java/com/example/server/messages/SendRoleMessage.java
@@ -3,6 +3,16 @@ package com.example.server.messages;
 public class SendRoleMessage extends BaseMessage {
     private String text;
 
+    private int lobbyId;
+
+    public int getLobbyId() {
+        return lobbyId;
+    }
+
+    public void setLobbyId(int lobbyId) {
+        this.lobbyId = lobbyId;
+    }
+
     public String getText() {
         return text;
     }
