741ffdf896c89e382b943fc692c51ae4d2811647
Poetzi <49530417+Poetzi@users.noreply.github.com>
2020-06-08 16:39:08 +0200

    BoardGameEngineImpl is now a Singleton Pattern!

--- server/src/main/java/com/example/server/game/boardGameEngine/implementation/BoardGameEngineImpl.java
+++ server/src/main/java/com/example/server/game/boardGameEngine/implementation/BoardGameEngineImpl.java
@@ -23,23 +23,35 @@ public class BoardGameEngineImpl implements BoardGameEngine {
 
     private Player[] players = new Player[6];
     private int numberOfPlayers = 0;
-    private int maxRounds=24;
+    private int maxRounds = 24;
     private int actualRound;
-    private GameBoard gameBoard= new GameBoardImpl();
+    private GameBoard gameBoard = new GameBoardImpl();
     private int numberOfFields;
     private Lobby lobby;
-    private  int mrXId;
+    private int mrXId;
+    // Singleton
+    private static BoardGameEngineImpl boardGameEngine;
 
+    // private Konstruktor
+    private BoardGameEngineImpl() {
+    }
 
+    // Singleton wird zurückgegeben
+    public static BoardGameEngineImpl getInstance() {
+        if (BoardGameEngineImpl.boardGameEngine == null) {
+            BoardGameEngineImpl.boardGameEngine = new BoardGameEngineImpl();
+        }
+        return BoardGameEngineImpl.boardGameEngine;
+    }
 
     @Override
-    public void addDetektiv(String name ,int id) {
+    public void addDetektiv(String name, int id) {
         players[id] = new DetectiveImpl(name, id);
         numberOfPlayers++;
     }
 
     @Override
-    public void addMrX(String name ,int id) {
+    public void addMrX(String name, int id) {
         players[id] = new MrXImpl(name, id);
         mrXId = id;
         numberOfPlayers++;
@@ -47,122 +59,119 @@ public class BoardGameEngineImpl implements BoardGameEngine {
 
     @Override
     public void setupNewGame() {
-        gameBoard.addFieldWithTransition(2,3,"taxi");
-        gameBoard.addFieldWithTransition(2,7,"bus");
-        gameBoard.addFieldWithTransition(2,7,"ubahn");
+        gameBoard.addFieldWithTransition(2, 3, "taxi");
+        gameBoard.addFieldWithTransition(2, 7, "bus");
+        gameBoard.addFieldWithTransition(2, 7, "ubahn");
 
-        gameBoard.addFieldWithTransition(3,1,"bus");
-        gameBoard.addFieldWithTransition(3,1,"taxi");
-        gameBoard.addFieldWithTransition(3,5,"taxi");
+        gameBoard.addFieldWithTransition(3, 1, "bus");
+        gameBoard.addFieldWithTransition(3, 1, "taxi");
+        gameBoard.addFieldWithTransition(3, 5, "taxi");
 
-        gameBoard.addFieldWithTransition(5,12,"taxi");
+        gameBoard.addFieldWithTransition(5, 12, "taxi");
 
-        gameBoard.addFieldWithTransition(12,1,"bus");
-        gameBoard.addFieldWithTransition(12,16,"bus");
+        gameBoard.addFieldWithTransition(12, 1, "bus");
+        gameBoard.addFieldWithTransition(12, 16, "bus");
 
-        gameBoard.addFieldWithTransition(1,7,"bus");
-        gameBoard.addFieldWithTransition(1,13,"taxi");
-        gameBoard.addFieldWithTransition(1,12,"bus");
+        gameBoard.addFieldWithTransition(1, 7, "bus");
+        gameBoard.addFieldWithTransition(1, 13, "taxi");
+        gameBoard.addFieldWithTransition(1, 12, "bus");
 
-        gameBoard.addFieldWithTransition(7,6,"ubahn");
-        gameBoard.addFieldWithTransition(7,8,"bus");
+        gameBoard.addFieldWithTransition(7, 6, "ubahn");
+        gameBoard.addFieldWithTransition(7, 8, "bus");
 
-        gameBoard.addFieldWithTransition(8,9,"taxi");
+        gameBoard.addFieldWithTransition(8, 9, "taxi");
 
-        gameBoard.addFieldWithTransition(6,9,"taxi");
-        gameBoard.addFieldWithTransition(6,9,"bus");
-        gameBoard.addFieldWithTransition(6,13,"bus");
-        gameBoard.addFieldWithTransition(6,13,"taxi");
-        gameBoard.addFieldWithTransition(6,14,"ubahn");
+        gameBoard.addFieldWithTransition(6, 9, "taxi");
+        gameBoard.addFieldWithTransition(6, 9, "bus");
+        gameBoard.addFieldWithTransition(6, 13, "bus");
+        gameBoard.addFieldWithTransition(6, 13, "taxi");
+        gameBoard.addFieldWithTransition(6, 14, "ubahn");
 
-        gameBoard.addFieldWithTransition(13,15,"taxi");
+        gameBoard.addFieldWithTransition(13, 15, "taxi");
 
-        gameBoard.addFieldWithTransition(15,16,"taxi");
-        gameBoard.addFieldWithTransition(15,14,"taxi");
-        gameBoard.addFieldWithTransition(15,19,"bus");
+        gameBoard.addFieldWithTransition(15, 16, "taxi");
+        gameBoard.addFieldWithTransition(15, 14, "taxi");
+        gameBoard.addFieldWithTransition(15, 19, "bus");
 
-        gameBoard.addFieldWithTransition(14,20,"ubahn");
-        gameBoard.addFieldWithTransition(14,25,"taxi");
-        gameBoard.addFieldWithTransition(14,25,"bus");
+        gameBoard.addFieldWithTransition(14, 20, "ubahn");
+        gameBoard.addFieldWithTransition(14, 25, "taxi");
+        gameBoard.addFieldWithTransition(14, 25, "bus");
 
-        gameBoard.addFieldWithTransition(16,18,"taxi");
+        gameBoard.addFieldWithTransition(16, 18, "taxi");
 
-        gameBoard.addFieldWithTransition(25,9,"taxi");
-        gameBoard.addFieldWithTransition(25,21,"taxi");
-        gameBoard.addFieldWithTransition(25,11,"bus");
+        gameBoard.addFieldWithTransition(25, 9, "taxi");
+        gameBoard.addFieldWithTransition(25, 21, "taxi");
+        gameBoard.addFieldWithTransition(25, 11, "bus");
 
-        gameBoard.addFieldWithTransition(9,10,"bus");
+        gameBoard.addFieldWithTransition(9, 10, "bus");
 
-        gameBoard.addFieldWithTransition(10,11,"taxi");
+        gameBoard.addFieldWithTransition(10, 11, "taxi");
 
-        gameBoard.addFieldWithTransition(11,33,"taxi");
-        gameBoard.addFieldWithTransition(11,22,"taxi");
+        gameBoard.addFieldWithTransition(11, 33, "taxi");
+        gameBoard.addFieldWithTransition(11, 22, "taxi");
 
-        gameBoard.addFieldWithTransition(33,32,"bus");
+        gameBoard.addFieldWithTransition(33, 32, "bus");
 
-        gameBoard.addFieldWithTransition(32,23,"taxi");
-        gameBoard.addFieldWithTransition(32,22,"bus");
-        gameBoard.addFieldWithTransition(32,22,"ubahn");
+        gameBoard.addFieldWithTransition(32, 23, "taxi");
+        gameBoard.addFieldWithTransition(32, 22, "bus");
+        gameBoard.addFieldWithTransition(32, 22, "ubahn");
 
-        gameBoard.addFieldWithTransition(22,21,"bus");
-        gameBoard.addFieldWithTransition(22,21,"ubahn");
-        gameBoard.addFieldWithTransition(22,24,"bus");
+        gameBoard.addFieldWithTransition(22, 21, "bus");
+        gameBoard.addFieldWithTransition(22, 21, "ubahn");
+        gameBoard.addFieldWithTransition(22, 24, "bus");
 
-        gameBoard.addFieldWithTransition(21,20,"bus");
-        gameBoard.addFieldWithTransition(21,20,"ubahn");
-        gameBoard.addFieldWithTransition(21,34,"bus");
+        gameBoard.addFieldWithTransition(21, 20, "bus");
+        gameBoard.addFieldWithTransition(21, 20, "ubahn");
+        gameBoard.addFieldWithTransition(21, 34, "bus");
 
-        gameBoard.addFieldWithTransition(20,19,"taxi");
-        gameBoard.addFieldWithTransition(20,19,"ubahn");
-        gameBoard.addFieldWithTransition(20,26,"ubahn");
+        gameBoard.addFieldWithTransition(20, 19, "taxi");
+        gameBoard.addFieldWithTransition(20, 19, "ubahn");
+        gameBoard.addFieldWithTransition(20, 26, "ubahn");
 
-        gameBoard.addFieldWithTransition(19,18,"bus");
-        gameBoard.addFieldWithTransition(19,18,"ubahn");
-        gameBoard.addFieldWithTransition(19,27,"taxi");
+        gameBoard.addFieldWithTransition(19, 18, "bus");
+        gameBoard.addFieldWithTransition(19, 18, "ubahn");
+        gameBoard.addFieldWithTransition(19, 27, "taxi");
 
-        gameBoard.addFieldWithTransition(23,44,"taxi");
-        gameBoard.addFieldWithTransition(23,24,"taxi");
+        gameBoard.addFieldWithTransition(23, 44, "taxi");
+        gameBoard.addFieldWithTransition(23, 24, "taxi");
 
-        gameBoard.addFieldWithTransition(24,45,"taxi");
-        gameBoard.addFieldWithTransition(45,44,"taxi");
+        gameBoard.addFieldWithTransition(24, 45, "taxi");
+        gameBoard.addFieldWithTransition(45, 44, "taxi");
 
-        gameBoard.addFieldWithTransition(45,34,"taxi");
-        gameBoard.addFieldWithTransition(45,30,"taxi");
+        gameBoard.addFieldWithTransition(45, 34, "taxi");
+        gameBoard.addFieldWithTransition(45, 30, "taxi");
 
-        gameBoard.addFieldWithTransition(34,29,"taxi");
-        gameBoard.addFieldWithTransition(34,26,"taxi");
-        gameBoard.addFieldWithTransition(27,26,"taxi");
+        gameBoard.addFieldWithTransition(34, 29, "taxi");
+        gameBoard.addFieldWithTransition(34, 26, "taxi");
+        gameBoard.addFieldWithTransition(27, 26, "taxi");
 
-        gameBoard.addFieldWithTransition(28,29,"bus");
-        gameBoard.addFieldWithTransition(30,29,"taxi");
+        gameBoard.addFieldWithTransition(28, 29, "bus");
+        gameBoard.addFieldWithTransition(30, 29, "taxi");
 
-        gameBoard.addFieldWithTransition(30,36,"ubahn");
-        gameBoard.addFieldWithTransition(30,31,"bus");
-        gameBoard.addFieldWithTransition(31,44,"taxi");
-        gameBoard.addFieldWithTransition(31,35,"bus");
-
-
-        gameBoard.addFieldWithTransition(37,29,"bus");
-
-        gameBoard.addFieldWithTransition(37,36,"taxi");
-        gameBoard.addFieldWithTransition(37,38,"taxi");
-        gameBoard.addFieldWithTransition(38,36,"taxi");
-        gameBoard.addFieldWithTransition(36,35,"ubahn");
-
-        gameBoard.addFieldWithTransition(43,35,"taxi");
-        gameBoard.addFieldWithTransition(43,35,"bus");
-
-        gameBoard.addFieldWithTransition(43,42,"taxi");
-
-        gameBoard.addFieldWithTransition(41,42,"bus");
-
-        gameBoard.addFieldWithTransition(41,40,"bus");
-
-        gameBoard.addFieldWithTransition(38,39,"bus");
-        gameBoard.addFieldWithTransition(40,39,"bus");
+        gameBoard.addFieldWithTransition(30, 36, "ubahn");
+        gameBoard.addFieldWithTransition(30, 31, "bus");
+        gameBoard.addFieldWithTransition(31, 44, "taxi");
+        gameBoard.addFieldWithTransition(31, 35, "bus");
 
 
+        gameBoard.addFieldWithTransition(37, 29, "bus");
 
+        gameBoard.addFieldWithTransition(37, 36, "taxi");
+        gameBoard.addFieldWithTransition(37, 38, "taxi");
+        gameBoard.addFieldWithTransition(38, 36, "taxi");
+        gameBoard.addFieldWithTransition(36, 35, "ubahn");
+
+        gameBoard.addFieldWithTransition(43, 35, "taxi");
+        gameBoard.addFieldWithTransition(43, 35, "bus");
+
+        gameBoard.addFieldWithTransition(43, 42, "taxi");
+
+        gameBoard.addFieldWithTransition(41, 42, "bus");
+
+        gameBoard.addFieldWithTransition(41, 40, "bus");
+
+        gameBoard.addFieldWithTransition(38, 39, "bus");
+        gameBoard.addFieldWithTransition(40, 39, "bus");
 
 
     }
@@ -171,18 +180,16 @@ public class BoardGameEngineImpl implements BoardGameEngine {
     public void startGame() {
 
 
-
         // Aktuelle Runde wird auf 0 gesetzt
         actualRound = 0;
 
         // Runden werden solange ausgeführt bis die Maximale Rundenanzahl erreicht ist
-        for (int i = 0; i < maxRounds ; i++) {
+        for (int i = 0; i < maxRounds; i++) {
             playOneRound();
 
 
             // Wenn die Detektive gewonnen haben wird der Spielablauf beendet
-            if (checkWinningCondition())
-            {
+            if (checkWinningCondition()) {
                 break;
             }
 
@@ -194,7 +201,7 @@ public class BoardGameEngineImpl implements BoardGameEngine {
 
     @Override
     public void playOneRound() {
-        for (Player p:players) {
+        for (Player p : players) {
             drawForPlayer(p);
         }
     }
@@ -207,8 +214,7 @@ public class BoardGameEngineImpl implements BoardGameEngine {
         TurnMessage turnMessage;
 
         // Schleife wird solange ausgeführt bis en gültiger Zug vom Spieler kommt
-        while (drawValide == false)
-        {
+        while (drawValide == false) {
             /*
                Der Server holt sich vom Spieler Client die Karte die er einsetzen will
                und die Position zu der er ziehen möchte
@@ -239,8 +245,7 @@ public class BoardGameEngineImpl implements BoardGameEngine {
             Wenn der Zug gültig ist, wird die Positon des Spielers auf dem Gameboard gesetzt
             und an die anderen Spieler Clients weitergegeben
          */
-        if (drawValide)
-        {
+        if (drawValide) {
             gameBoard.setPositionOfPlayer(player.getId(), fieldToGo);
 
             /*
@@ -251,17 +256,16 @@ public class BoardGameEngineImpl implements BoardGameEngine {
     }
 
 
-
     @Override
     public boolean checkWinningCondition() {
         int misterX = mrXId;
 
-        if(maxRounds==actualRound){
+        if (maxRounds == actualRound) {
             System.out.println("Mister X won");
             return false;
         }
 
-        for (int i = 0; i <numberOfPlayers ; i++) {
+        for (int i = 0; i < numberOfPlayers; i++) {
             /*if(players.get(misterX) == players.get(i).currentPosition){ //Missing field from Player CLass
                 System.out.println("Mister X lost");
                 return true;
