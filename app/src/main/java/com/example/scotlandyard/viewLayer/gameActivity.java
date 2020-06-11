e22f7abee1e03890f4561ee9bd34f71d33f8b884
Poetzi <49530417+Poetzi@users.noreply.github.com>
2020-06-11 12:51:25 +0200

    gameActivity Kommentare ausgebessert!

--- app/src/main/java/com/example/scotlandyard/viewLayer/gameActivity.java
+++ app/src/main/java/com/example/scotlandyard/viewLayer/gameActivity.java
@@ -1,17 +1,10 @@
 package com.example.scotlandyard.viewLayer;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Looper;
 import android.view.Menu;
 import android.view.View;
@@ -19,9 +12,13 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.example.scotlandyard.Client.Messages.TurnMessage;
 import com.example.scotlandyard.R;
-import com.example.scotlandyard.modelLayer.TravelLog;
 import com.example.scotlandyard.presenterLayer.Presenter;
 import com.google.android.material.navigation.NavigationView;
 
